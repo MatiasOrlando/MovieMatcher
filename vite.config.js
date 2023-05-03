4e2d61e7795a17b04cb89d3e75664db1f12c07ba
MatiasOrlando <matiassorlando@gmail.com>
2023-05-04 01:50:52 +0200

    update vite.config.js done

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    fs: {
-      strict: false,
-    },
-    middleware: [
-      (req, res, next) => {
-        if (req.originalUrl !== "/" && !req.originalUrl.includes(".")) {
-          req.url = "/";
-        }
-        next();
-      },
-    ],
+    rewrites: [{ source: "/(.*)", destination: "/index.html" }],
   },
 });
